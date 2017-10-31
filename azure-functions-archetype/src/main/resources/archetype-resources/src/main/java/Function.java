8cb9e3bec20daaa74f820b29a377ab3b9d9f579f
Kevin Zhao <kevinzha@microsoft.com>
2017-10-31 16:25:49 +0800

    update maven plugin to 0.1.6

--- azure-functions-archetype/src/main/resources/archetype-resources/src/main/java/Function.java
+++ azure-functions-archetype/src/main/resources/archetype-resources/src/main/java/Function.java
@@ -1,15 +1,37 @@
 package $package;
 
 import com.microsoft.azure.serverless.functions.annotation.*;
-import com.microsoft.azure.serverless.functions.ExecutionContext;
+import com.microsoft.azure.serverless.functions.*;
 
 /**
- * Hello function with HTTP Trigger.
+ * Azure Functions with HTTP Trigger.
  */
 public class Function {
+    /**
+     * This function listens at endpoint "/api/hello". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/hello
+     * 2. curl {your host}/api/hello?name=HTTP%20Query
+     */
     @FunctionName("hello")
-    public String hello(@HttpTrigger(name = "req", methods = {"get", "post"}, authLevel = AuthorizationLevel.ANONYMOUS) String req,
-                        ExecutionContext context) {
-        return String.format("Hello, %s!", req);
+    public HttpResponseMessage hello(@HttpTrigger(name = "req", methods = {"get", "post"}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage request,
+                                     final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        // Parse query parameter
+        String name = request.getQueryParameters().get("name");
+
+        if (name == null) {
+            // Get request body
+            Object body = request.getBody();
+            if (body != null) {
+                name = body.toString();
+            }
+        }
+
+        if (name == null) {
+            return request.createResponse(400, "Please pass a name on the query string or in the request body");
+        } else {
+            return request.createResponse(200, "Hello, " + name);
+        }
     }
 }
--- azure-functions-archetype/src/main/resources/archetype-resources/src/test/java/FunctionTest.java
+++ azure-functions-archetype/src/main/resources/archetype-resources/src/test/java/FunctionTest.java
@@ -1,8 +1,18 @@
 package $package;
 
 import org.junit.Test;
+import com.microsoft.azure.serverless.functions.*;
 
-import static org.junit.Assert.assertEquals;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import static org.junit.Assert.assertSame;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
 
 /**
  * Unit test for Function class.
@@ -13,10 +23,23 @@ public class FunctionTest {
      */
     @Test
     public void testHello() throws Exception {
-        final Function function = new Function();
+        // Setup
+        final HttpRequestMessage req = mock(HttpRequestMessage.class);
 
-        final String ret = function.hello("function", null);
+        final Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("name", "Azure");
+        doReturn(queryParams).when(req).getQueryParameters();
 
-        assertEquals("Hello, function!", ret);
+        final HttpResponseMessage res = mock(HttpResponseMessage.class);
+        doReturn(res).when(req).createResponse(anyInt(), anyString());
+
+        final ExecutionContext context = mock(ExecutionContext.class);
+        doReturn(Logger.getGlobal()).when(context).getLogger();
+
+        // Invoke
+        final HttpResponseMessage ret = new Function().hello(req, context);
+
+        // Verify
+        assertSame(res, ret);
     }
 }
