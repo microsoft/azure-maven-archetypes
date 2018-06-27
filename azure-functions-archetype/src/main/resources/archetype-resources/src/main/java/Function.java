39675b851ccf19723bd325d9f896bad11989cfbf
Sheng Chen <sheche@microsoft.com>
2018-06-27 10:01:16 +0800

    update

--- azure-functions-archetype/src/main/resources/archetype-resources/src/main/java/Function.java
+++ azure-functions-archetype/src/main/resources/archetype-resources/src/main/java/Function.java
@@ -14,19 +14,19 @@ public class Function {
      * 2. curl {your host}/api/hello?name=HTTP%20Query
      */
     @FunctionName("HttpTrigger-Java")
-    public HttpResponseMessage<String> HttpTriggerJava(
-        @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
-        final ExecutionContext context) {
-    context.getLogger().info("Java HTTP trigger processed a request.");
+    public HttpResponseMessage HttpTriggerJava(
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
 
-    // Parse query parameter
-    String query = request.getQueryParameters().get("name");
-    String name = request.getBody().orElse(query);
+        // Parse query parameter
+        String query = request.getQueryParameters().get("name");
+        String name = request.getBody().orElse(query);
 
-    if (name == null) {
-        return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
-    } else {
-        return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+        if (name == null) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
+        } else {
+            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+        }
     }
-}
 }
