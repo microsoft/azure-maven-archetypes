5b8a2965a937736dedd54bbda33ca64f5b08369b
Hanxiao Liu <hanli@microsoft.com>
2018-09-27 11:20:27 +0800

    change auth level (#72)

--- azure-functions-archetype/src/main/resources/archetype-resources/src/main/java/Function.java
+++ azure-functions-archetype/src/main/resources/archetype-resources/src/main/java/Function.java
@@ -10,12 +10,13 @@ import com.microsoft.azure.functions.*;
 public class Function {
     /**
      * This function listens at endpoint "/api/HttpTrigger-Java". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpTrigger-Java
-     * 2. curl {your host}/api/HttpTrigger-Java?name=HTTP%20Query
+     * 1. curl -d "HTTP Body" {your host}/api/HttpTrigger-Java&code={your function key}
+     * 2. curl "{your host}/api/HttpTrigger-Java?name=HTTP%20Query&code={your function key}"
+     * Function Key is not needed when running locally, to invoke HttpTrigger deployed to Azure, see here(https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-http-webhook#authorization-keys) on how to get function key for your app.
      */
     @FunctionName("HttpTrigger-Java")
     public HttpResponseMessage run(
-            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
