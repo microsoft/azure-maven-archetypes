02c96f0077b7d229011aa399a6228413ab68610c
Pragna Gopa <pgopa@microsoft.com>
2018-10-28 08:40:19 -0700

    Http mock update (#79)

--- azure-functions-archetype/src/main/resources/archetype-resources/src/test/java/HttpResponseMessageMock.java
+++ azure-functions-archetype/src/main/resources/archetype-resources/src/test/java/HttpResponseMessageMock.java
@@ -5,55 +5,76 @@ import com.microsoft.azure.functions.*;
 import java.util.Map;
 
 /**
- * The mock for HttpResponseMessage, can be used in unit tests to verify if the returned response by HTTP trigger function is correct or not.
+ * The mock for HttpResponseMessage, can be used in unit tests to verify if the
+ * returned response by HTTP trigger function is correct or not.
  */
 public class HttpResponseMessageMock implements HttpResponseMessage {
-    private HttpStatus status;
-    private Map<String, String> headers;
+    private int httpStatusCode;
+    private HttpStatusType httpStatus;
     private Object body;
+    private Map<String, String> headers;
 
-    public HttpResponseMessageMock(HttpStatus status, Map<String, String> headers, Object body) {
-        this.status = status;
+    public HttpResponseMessageMock(HttpStatusType status, Map<String, String> headers, Object body) {
+        this.httpStatus = status;
+        this.httpStatusCode = status.value();
         this.headers = headers;
         this.body = body;
     }
-    
+
     @Override
-    public HttpStatus getStatus() {
-        return this.status;
+    public HttpStatusType getStatus() {
+        return this.httpStatus;
     }
+
+    @Override
+    public int getStatusCode() {
+        return httpStatusCode;
+    }
+
     @Override
     public String getHeader(String key) {
         return this.headers.get(key);
     }
+
     @Override
     public Object getBody() {
         return this.body;
     }
 
     public static class HttpResponseMessageBuilderMock implements HttpResponseMessage.Builder {
-        private HttpStatus status;
-        private Map<String, String> headers;
         private Object body;
+        private int httpStatusCode;
+        private Map<String, String> headers;
+        private HttpStatusType httpStatus;
 
-        @Override
-        public HttpResponseMessage.Builder status(HttpStatus status) {
-            this.status = status;
+        public Builder status(HttpStatus status) {
+            this.httpStatusCode = status.value();
+            this.httpStatus = status;
             return this;
         }
+
+        @Override
+        public Builder status(HttpStatusType httpStatusType) {
+            this.httpStatusCode = httpStatusType.value();
+            this.httpStatus = httpStatusType;
+            return this;
+        }
+
         @Override
         public HttpResponseMessage.Builder header(String key, String value) {
             this.headers.put(key, value);
             return this;
         }
+
         @Override
         public HttpResponseMessage.Builder body(Object body) {
             this.body = body;
             return this;
         }
+
         @Override
         public HttpResponseMessage build() {
-            return new HttpResponseMessageMock(this.status, this.headers, this.body);
+            return new HttpResponseMessageMock(this.httpStatus, this.headers, this.body);
         }
     }
 }
