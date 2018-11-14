39bb9a11097db90e0ca8e19d01554c9ec4bb0f19
hanxiao <hanli@microsoft.com>
2018-11-14 15:25:19 +0800

    Resolve comments

--- e2etest.ps1
+++ e2etest.ps1
@@ -1,30 +1,51 @@
-$base = pwd;
+# Define the functions
+Function RemoveFileIfExist($fileName){
+    if(Test-Path $fileName){
+        Remove-Item -Force $fileName
+    }
+}
+
+Function RemoveFolderIfExist($folderName){
+    if(Test-Path $folderName){
+        Remove-Item -Recurse -Force $folderName
+    }
+}
+
+Function DownloadFileFromUrl($url,$destination){
+    $wc = New-Object System.Net.WebClient
+    $wc.DownloadFile($Env:FUNCTIONCLI_URL, $functionCLIZipPath)
+}
+
+#Scripts
+$base = pwd
+$functionCLIPath = "$base\Azure.Functions.Cli"
+$functionCLIZipPath = "$base\Azure.Functions.Cli.zip"
+
 # Download Functions Core Tools
-Remove-Item -Force "$base\Azure.Functions.Cli.zip" -ErrorAction Ignore
-Remove-Item -Recurse -Force "$base\Azure.Functions.Cli" -ErrorAction Ignore
-Write-Host "Downloading Functions Host...."
-$url = "https://functionsclibuilds.blob.core.windows.net/builds/2/latest/Azure.Functions.Cli.win-x64.zip"
-$output = "$base\Azure.Functions.Cli.zip"
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $output)
-Expand-Archive "$base\Azure.Functions.Cli.zip" -DestinationPath "$base\Azure.Functions.Cli"
-$Env:Path = $Env:Path+";$base\Azure.Functions.Cli"
+RemoveFileIfExist $functionCLIZipPath
+RemoveFolderIfExist $functionCLIPath
+DownloadFileFromUrl $Env:FUNCTIONCLI_URL $functionCLIZipPath
+Expand-Archive $functionCLIZipPath -DestinationPath $functionCLIPath
+$Env:Path = $Env:Path+";$functionCLIPath"
+
 # Install maven plguin archetype
-$archetypePom = Get-Content ".\azure-functions-archetype\pom.xml" -Raw
-$archetypePom -match "<version>(.*)</version>"
-$atchetypeVersion = $matches[1]
 mvn clean install
+
 # Generate function project through archetype
-Remove-Item -Recurse -Force ".\e2etestproject" -ErrorAction Ignore
-mkdir e2etestproject
-cd e2etestproject
+$testProjectBaseFolder = ".\testprojects"
+# Get the version of archetype and set archetypeVersion or maven will use the repository version to create project
+$atchetypeVersion = ([xml](gc ".\azure-functions-archetype\pom.xml")).project.version 
+RemoveFolderIfExist $testProjectBaseFolder
+mkdir $testProjectBaseFolder
+cd $testProjectBaseFolder
 mvn archetype:generate -DarchetypeCatalog="local" -DarchetypeGroupId="com.microsoft.azure" -DarchetypeArtifactId="azure-functions-archetype" -DarchetypeVersion="$atchetypeVersion" -DgroupId="com.microsoft" -DartifactId="e2etestproject" -Dversion="1.0-SNAPSHOT" -Dpackage="com.microsoft" -DappRegion="westus" -DresourceGroup="e2etest-java-functions-group" -DappName="e2etest-java-functions" -B
 mvn -f ".\e2etestproject\pom.xml" clean package
 cd ..
+
 # Run function host
 $Env:FUNCTIONS_WORKER_RUNTIME = "java"
 $Env:AZURE_FUNCTIONS_ENVIRONMENT = "development"
-$Env:AzureWebJobsScriptRoot = "$base\e2etestproject\e2etestproject\target\azure-functions\e2etest-java-functions"
-$proc = start-process -filepath "$base\Azure.Functions.Cli\func.exe" -WorkingDirectory "$Env:AzureWebJobsScriptRoot" -ArgumentList "host start" -PassThru
+$Env:AzureWebJobsScriptRoot = "$base\$testProjectBaseFolder\e2etestproject\target\azure-functions\e2etest-java-functions"
+$proc = start-process -filepath "$functionCLIPath\func.exe" -WorkingDirectory "$Env:AzureWebJobsScriptRoot" -ArgumentList "host start" -RedirectStandardOutput "output.txt" -RedirectStandardError "error.txt" -PassThru
 Start-Sleep -s 30
 return $proc
