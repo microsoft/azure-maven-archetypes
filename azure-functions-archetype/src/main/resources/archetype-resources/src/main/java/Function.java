55abb6a336bf20e843b4c9da2f61f87ac76e7740
Hanxiao Liu <hanli@microsoft.com>
2018-12-18 17:23:06 +0800

    Split the comments for it is too long (#100)

--- azure-functions-archetype/src/main/resources/archetype-resources/src/main/java/Function.java
+++ azure-functions-archetype/src/main/resources/archetype-resources/src/main/java/Function.java
@@ -12,7 +12,8 @@ public class Function {
      * This function listens at endpoint "/api/HttpTrigger-Java". Two ways to invoke it using "curl" command in bash:
      * 1. curl -d "HTTP Body" {your host}/api/HttpTrigger-Java&code={your function key}
      * 2. curl "{your host}/api/HttpTrigger-Java?name=HTTP%20Query&code={your function key}"
-     * Function Key is not needed when running locally, to invoke HttpTrigger deployed to Azure, see here(https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-http-webhook#authorization-keys) on how to get function key for your app.
+     * Function Key is not needed when running locally, it is used to invoke function deployed to Azure.
+     * More details: https://aka.ms/functions_authorization_keys
      */
     @FunctionName("HttpTrigger-Java")
     public HttpResponseMessage run(
