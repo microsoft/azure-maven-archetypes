da01783b394f055c9209be68562ae84baf452f78
Hanxiao Liu <hanli@microsoft.com>
2019-12-16 13:28:59 +0800

    Use Boolean.valueOf() instead of String compare

--- azure-functions-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ azure-functions-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -2,7 +2,7 @@ import java.nio.file.Files
 
 def isDocker = request.getProperties().get("docker")
 // Remove Dockerfile if use didn't set -Ddocker
-if (isDocker == null || !isDocker.equals("true")) {
+if (!Boolean.valueOf(isDocker)) {
     def dockerFile = "Dockerfile";
     def rootDir = new File(request.getOutputDirectory() + "/" + request.getArtifactId())
     Files.deleteIfExists(new File(rootDir, dockerFile).toPath())
