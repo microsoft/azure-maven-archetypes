5294824e43bf69dd4d32733a394fefca1ea9e341
Hanxiao Liu <hanli@microsoft.com>
2019-12-17 13:10:58 +0800

    Change to docker runtime when user specify -Ddocker

--- azure-functions-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ azure-functions-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,9 +1,16 @@
 import java.nio.file.Files
+import java.nio.file.StandardCopyOption;
 
 def isDocker = request.getProperties().get("docker")
-// Remove Dockerfile if use didn't set -Ddocker
-if (!Boolean.valueOf(isDocker)) {
-    def dockerFile = "Dockerfile";
-    def rootDir = new File(request.getOutputDirectory() + "/" + request.getArtifactId())
+def dockerFile = "Dockerfile"
+def pomFile = "pom.xml"
+def dockerPomFile = "pom-docker.xml"
+def rootDir = new File(request.getOutputDirectory() + "/" + request.getArtifactId())
+if (Boolean.valueOf(isDocker)) {
+    // For docker, replace origin pom.xml with pom-docker.xml
+    Files.move(new File(rootDir, dockerPomFile).toPath(), new File(rootDir, pomFile).toPath(), StandardCopyOption.REPLACE_EXISTING)
+} else {
+    // Otherwise, remove Dockerfile and pom-docker.xml
     Files.deleteIfExists(new File(rootDir, dockerFile).toPath())
+    Files.deleteIfExists(new File(rootDir, dockerPomFile).toPath())
 }
