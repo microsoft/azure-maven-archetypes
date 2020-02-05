ddf532bdc5b1ceb086a4bb473525be4f9e0732cc
Glenn Gailey <ggailey777@hotmail.com>
2020-02-04 18:19:43 -0800

    Change the default HTTP trigger name to HttpExample (#130)
    This aligns the Java to other quickstarts and lets us add a Java pivot in the existing quickstarts.

--- azure-functions-archetype/src/main/resources/archetype-resources/src/main/java/Function.java
+++ azure-functions-archetype/src/main/resources/archetype-resources/src/main/java/Function.java
@@ -9,15 +9,13 @@ import com.microsoft.azure.functions.*;
  */
 public class Function {
     /**
-     * This function listens at endpoint "/api/HttpTrigger-Java". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpTrigger-Java&code={your function key}
-     * 2. curl "{your host}/api/HttpTrigger-Java?name=HTTP%20Query&code={your function key}"
-     * Function Key is not needed when running locally, it is used to invoke function deployed to Azure.
-     * More details: https://aka.ms/functions_authorization_keys
+     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
+     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
+     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
      */
-    @FunctionName("HttpTrigger-Java")
+    @FunctionName("HttpExample")
     public HttpResponseMessage run(
-            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
