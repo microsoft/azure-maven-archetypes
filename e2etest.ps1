d8bdddd812f7ca4104c7b986d9546df8281e8c39
Hanxiao Liu <hanli@microsoft.com>
2020-02-21 15:26:18 +0800

    Update CI script (#133)

--- e2etest.ps1
+++ e2etest.ps1
@@ -34,18 +34,19 @@ mvn clean install
 # Generate function project through archetype
 $testProjectBaseFolder = ".\testprojects"
 # Get the version of archetype and set archetypeVersion in following steps
-$atchetypeVersion = ([xml](gc ".\azure-functions-archetype\pom.xml")).project.version 
+$archetypeVersion = ([xml](gc ".\azure-functions-archetype\pom.xml")).project.version 
 RemoveFolderIfExist $testProjectBaseFolder
 mkdir $testProjectBaseFolder
 cd $testProjectBaseFolder
-mvn archetype:generate -DarchetypeCatalog="local" -DarchetypeGroupId="com.microsoft.azure" -DarchetypeArtifactId="azure-functions-archetype" -DarchetypeVersion="$atchetypeVersion" -DgroupId="com.microsoft" -DartifactId="e2etestproject" -Dversion="1.0-SNAPSHOT" -Dpackage="com.microsoft" -DappRegion="westus" -DresourceGroup="e2etest-java-functions-group" -DappName="e2etest-java-functions" -B
+mvn archetype:generate -DarchetypeCatalog="local" -DarchetypeGroupId="com.microsoft.azure" -DarchetypeArtifactId="azure-functions-archetype" -DarchetypeVersion="$archetypeVersion" -DgroupId="com.microsoft" -DartifactId="e2etestproject" -Dversion="1.0-SNAPSHOT" -Dpackage="com.microsoft" -B
+$testFunctionAppName = ([xml](gc ".\e2etestproject\pom.xml")).project.properties.functionAppName
 mvn -f ".\e2etestproject\pom.xml" clean package
 cd ..
 
 # Run function host
 $Env:FUNCTIONS_WORKER_RUNTIME = "java"
 $Env:AZURE_FUNCTIONS_ENVIRONMENT = "development"
-$Env:AzureWebJobsScriptRoot = "$base\$testProjectBaseFolder\e2etestproject\target\azure-functions\e2etest-java-functions"
+$Env:AzureWebJobsScriptRoot = "$base\$testProjectBaseFolder\e2etestproject\target\azure-functions\$testFunctionAppName"
 $proc = start-process -filepath "$functionCLIPath\func.exe" -WorkingDirectory "$Env:AzureWebJobsScriptRoot" -ArgumentList "host start" -RedirectStandardOutput "output.txt" -RedirectStandardError "error.txt" -PassThru
 Start-Sleep -s 30
 return $proc
