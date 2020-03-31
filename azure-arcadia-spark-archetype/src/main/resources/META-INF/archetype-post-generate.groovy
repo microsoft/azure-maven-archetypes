aea567400dc01aebb66f7196292b6036fd77eb8f
Rui Fang <rufan@microsoft.com>
2020-03-31 15:49:01 +0800

    Remove useless file deletion code since the source file doesn't exist now

--- azure-arcadia-spark-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ azure-arcadia-spark-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -6,11 +6,8 @@ if (!Boolean.valueOf(needSampleCode)) {
     def packageRelativePath = request.getGroupId().replace('.', '/')
     def scalaCodesDir = new File("$rootPath/src/main/scala/$packageRelativePath")
     def javaCodesDir = new File("$rootPath/src/main/java/$packageRelativePath")
-    def dataDir = new File("$rootPath/data/__default__/")
     cleanupFolder(scalaCodesDir)
     cleanupFolder(javaCodesDir)
-    cleanupFolder(dataDir)
-    new File("${dataDir.path}/user/current").mkdirs()
 }
 
 // Clean up all the contents in the "dir" not including "dir" itself
