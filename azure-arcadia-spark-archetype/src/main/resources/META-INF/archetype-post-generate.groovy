14e8cc8fec069005463be5bf567b4414d67e59ba
Rui Fang <rufan@microsoft.com>
2020-04-06 23:56:27 +0800

    Fix wrong package name and create empty folder for mocked filesystem

--- azure-arcadia-spark-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ azure-arcadia-spark-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -1,11 +1,13 @@
 import java.nio.file.Files
 
 def needSampleCode = request.getProperties().get("needSampleCode")
+def rootPath = "${request.getOutputDirectory()}/${request.getArtifactId()}"
+// Create an empty folder to unblock Spark local run feature in azure-toolikit-for-intellij plugin
+new File("$rootPath/data/__default__/user/current").mkdirs()
+
 if (!Boolean.valueOf(needSampleCode)) {
-    def rootPath = "${request.getOutputDirectory()}/${request.getArtifactId()}"
-    def packageRelativePath = request.getGroupId().replace('.', '/')
-    def scalaCodesDir = new File("$rootPath/src/main/scala/$packageRelativePath")
-    def javaCodesDir = new File("$rootPath/src/main/java/$packageRelativePath")
+    def scalaCodesDir = new File("$rootPath/src/main/scala/${request.getPackage()}")
+    def javaCodesDir = new File("$rootPath/src/main/java/${request.getPackage()}")
     cleanupFolder(scalaCodesDir)
     cleanupFolder(javaCodesDir)
 }
