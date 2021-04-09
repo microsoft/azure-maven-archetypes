2ff090bae4c6d2e315a27f4a1b5f8721782fabc2
Hanxiao Liu <hanli@microsoft.com>
2021-04-09 17:10:59 +0800

    Throwing exception instead of show warning in post script

--- azure-functions-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ azure-functions-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -11,6 +11,12 @@ if (!Boolean.valueOf(isDocker)) {
 }
 
 def trigger = request.getProperties().get("trigger");
+if ("HttpTrigger".equalsIgnoreCase(trigger)) {
+    return
+}
+// Remove origin source dir which contains unused test cases
+def sourceFolder = new File(rootDir, "src")
+sourceFolder.deleteDir()
 // todo: remove the parameter with default values, may need to update maven plugin
 def templateMap = [
         "BlobTrigger"           : "-Dfunctions.template=BlobTrigger -Dconnection=\"<connection>\" -Dpath=mycontainer",
@@ -22,28 +28,23 @@ def templateMap = [
         "ServiceBusQueueTrigger": "-Dfunctions.template=ServiceBusQueueTrigger -Dconnection=\"<connection>\" -DqueueName=mysbqueue",
         "ServiceBusTopicTrigger": "-Dfunctions.template=ServiceBusTopicTrigger -Dconnection=\"<connection>\" -DtopicName=mysbtopic -DsubscriptionName=mysubscription",
 ];
-if (!"HttpTrigger".equalsIgnoreCase(trigger)) {
-    println("Generating trigger from template, please replace the values with placeholder in annotation with real value if necessary")
-    def triggerParameter = templateMap.get(trigger)
-    if (triggerParameter == null) {
-        println(String.format("Invalid trigger type, supported values are %s and HttpTrigger, using HttpTrigger by default", String.join(",", templateMap.keySet())));
-        return
-    }
-    def sourceFolder = new File(rootDir, "src")
-    sourceFolder.deleteDir(); // Remove origin source dir which contains unused test cases
-    def isWindows = System.properties['os.name'].toLowerCase().contains('windows')
-    def starter = isWindows ? "cmd.exe" : "/bin/sh"
-    def switcher = isWindows ? "/c" : "-c"
-    def command = "mvn azure-functions:add -f ${pom.getAbsolutePath()} -Dfunctions.package=${request.getProperties().get("groupId")} -Dfunctions.name=Function ${triggerParameter} -B"
-    if (!isWindows) {
-        command = command.replace("\$", "\\\$")
-    }
-    def output = new StringBuilder()
-    def proc = [starter, switcher, command].execute();
-    proc.consumeProcessOutput(output, output)
-    proc.waitForOrKill(60 * 1000); // wait for 60s
-    if (proc.exitValue() != 0 || output == null || !output.contains("BUILD SUCCESS")) {
-        println("Failed to generate target trigger, please run `mvn azure-functions:add` manually in project root")
-        println("Output: \n${output}")
-    }
+println("Generating trigger from template, please replace the values with placeholder in annotation with real value if necessary")
+def triggerParameter = templateMap.get(trigger)
+if (triggerParameter == null) {
+    throw new RuntimeException(String.format("Invalid trigger type, supported values are %s and HttpTrigger", String.join(",", templateMap.keySet())))
+}
+def isWindows = System.properties['os.name'].toLowerCase().contains('windows')
+def starter = isWindows ? "cmd.exe" : "/bin/sh"
+def switcher = isWindows ? "/c" : "-c"
+def command = "mvn azure-functions:add -f ${pom.getAbsolutePath()} -Dfunctions.package=${request.getProperties().get("groupId")} -Dfunctions.name=Function ${triggerParameter} -B"
+if (!isWindows) {
+    command = command.replace("\$", "\\\$")
+}
+def output = new StringBuilder()
+def proc = [starter, switcher, command].execute();
+proc.consumeProcessOutput(output, output)
+proc.waitForOrKill(60 * 1000); // wait for 60s
+if (proc.exitValue() != 0 || output == null || !output.contains("BUILD SUCCESS")) {
+    println("${output}")
+    throw new RuntimeException("Failed to generate target trigger, please run `mvn azure-functions:add` manually in project root")
 }
