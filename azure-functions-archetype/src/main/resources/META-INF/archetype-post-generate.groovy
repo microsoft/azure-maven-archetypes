c71fbda4d366086089af3f2f48945951e7f1f228
Hanxiao Liu <hanli@microsoft.com>
2021-04-12 21:11:45 +0800

    Make -Dtriger case insensitive and support parameter without 'Trigger'

--- azure-functions-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++ azure-functions-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -2,16 +2,17 @@ import java.nio.file.Files
 import java.nio.file.StandardCopyOption;
 import java.nio.file.Paths;
 
-def isDocker = request.getProperties().get("docker")
-def artifactId = request.getProperties().get("artifactId")
-def dockerFile = "Dockerfile"
 def rootDir = Paths.get(request.getOutputDirectory(), artifactId).toFile()
+
+def isDocker = request.getProperties().get("docker")
 if (!Boolean.valueOf(isDocker)) {
+    def artifactId = request.getProperties().get("artifactId")
+    def dockerFile = "Dockerfile"
     Files.deleteIfExists(new File(rootDir, dockerFile).toPath()) // Delete Dockerfile for none-docker project
 }
 
 def trigger = request.getProperties().get("trigger");
-if ("HttpTrigger".equalsIgnoreCase(trigger)) {
+if ("HttpTrigger".equalsIgnoreCase(trigger) || "Http".equalsIgnoreCase(trigger)) {
     return
 }
 // Remove origin source dir which contains unused test cases
@@ -28,15 +29,16 @@ def templateMap = [
         "ServiceBusQueueTrigger": "-Dfunctions.template=ServiceBusQueueTrigger -Dconnection=\"<connection>\" -DqueueName=mysbqueue",
         "ServiceBusTopicTrigger": "-Dfunctions.template=ServiceBusTopicTrigger -Dconnection=\"<connection>\" -DtopicName=mysbtopic -DsubscriptionName=mysubscription",
 ];
+def triggerParameter = templateMap.keySet().stream()
+        .filter({ key -> key.equalsIgnoreCase(trigger) || key.substring(0, key.lastIndexOf("Trigger")).equalsIgnoreCase(trigger) }).findFirst()
+        .map(templateMap.&get)
+        .orElseThrow({ ->  new RuntimeException(String.format("Invalid trigger type `%s`, supported values are %s and HttpTrigger", trigger, String.join(",", templateMap.keySet()))) })
 println("Generating trigger from template, please replace the values with placeholder in annotation with real value if necessary")
-def triggerParameter = templateMap.get(trigger)
-if (triggerParameter == null) {
-    throw new RuntimeException(String.format("Invalid trigger type, supported values are %s and HttpTrigger", String.join(",", templateMap.keySet())))
-}
+def pomFile = new File(rootDir, "pom.xml")
 def isWindows = System.properties['os.name'].toLowerCase().contains('windows')
 def starter = isWindows ? "cmd.exe" : "/bin/sh"
 def switcher = isWindows ? "/c" : "-c"
-def command = "mvn azure-functions:add -f ${pom.getAbsolutePath()} -Dfunctions.package=${request.getProperties().get("groupId")} -Dfunctions.name=Function ${triggerParameter} -B"
+def command = "mvn azure-functions:add -f ${pomFile.getAbsolutePath()} -Dfunctions.package=${request.getProperties().get("groupId")} -Dfunctions.name=Function ${triggerParameter} -B"
 if (!isWindows) {
     command = command.replace("\$", "\\\$")
 }
